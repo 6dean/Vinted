0b3f10e46124a038d5389a79576d27254676c1bd
6dean <ferdjallahrockdean@gmail.com>
2022-10-21 17:46:55 +0200

    new config API

--- Utilities/cloudinary.js
+++ Utilities/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-  API_KEY: process.env.CLOUDINARY_API_KEY,
-  API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET,
   SECURE: true,
 });
 
