57ea4858b78d567dfda1b659b2c1b633f1b4af0f
6dean <ferdjallahrockdean@gmail.com>
2022-11-13 15:25:07 +0100

    update avatar

--- Routes/signup.js
+++ Routes/signup.js
@@ -54,8 +54,8 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
       const validSignup = {
         id: newSignup._id,
         token: token,
+        email: email,
         account: { username: username },
-        avatar: { secure_url: upLoad.secure_url },
       };
 
       res.status(200).json(validSignup);
