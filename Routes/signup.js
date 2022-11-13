cd8bb109236b3fa67ba3e7580cdbd1601af6245f
6dean <ferdjallahrockdean@gmail.com>
2022-11-13 15:34:52 +0100

    up

--- Routes/signup.js
+++ Routes/signup.js
@@ -29,8 +29,7 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
       const token = uid2(16);
 
       if (req.files.avatar === undefined) {
-        req.files.avatar ===
-          "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
+        req.files.avatar === "null";
       } else {
         const raw = req.files.avatar;
         const IMG = convertToBase64(raw);
