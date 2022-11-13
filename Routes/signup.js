3d4ca0211f70bfda81c0611db8813c3b8ecd4841
6dean <ferdjallahrockdean@gmail.com>
2022-11-13 20:07:56 +0100

    update

--- Routes/signup.js
+++ Routes/signup.js
@@ -29,13 +29,12 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
       const hash = SHA256(salt + password).toString(encBase64);
       const token = uid2(16);
 
-      if (avatar) {
-        const IMG = convertToBase64(avatar);
-      }
-
-      const upLoad = await cloudinary.uploader.upload(IMG, {
-        folder: "/Vinted/avatars",
-      });
+      let upLoad = "";
+      avatar
+        ? (upLoad = await cloudinary.uploader.upload(convertToBase64(avatar), {
+            folder: "/Vinted/avatars",
+          }))
+        : (upLoad = "none");
 
       const newSignup = new User({
         email: email,
