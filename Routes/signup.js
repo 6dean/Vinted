5ee9be0ac010d13d9abd30ed08c856ab3da0f751
6dean <ferdjallahrockdean@gmail.com>
2022-11-14 10:04:21 +0100

    first version

--- Routes/signup.js
+++ Routes/signup.js
@@ -24,41 +24,24 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
       const hash = SHA256(salt + password).toString(encBase64);
       const token = uid2(16);
 
-      if (req.files.avatar !== null) {
-        const convertToBase64 = (file) => {
-          return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
-        };
+      const raw = req.files.avatar;
+      const IMG = convertToBase64(raw);
+      const upLoad = await cloudinary.uploader.upload(IMG, {
+        folder: "/Vinted/avatars",
+      });
 
-        await cloudinary.uploader.upload(convertToBase64(avatar), {
-          folder: "/Vinted/avatars",
-        });
-
-        const newSignup = new User({
-          email: email,
-          account: {
-            username: username,
-          },
-          newsletter: newsletter,
-          token: token,
-          hash: hash,
-          salt: salt,
-          avatar: { secure_url: upLoad.secure_url },
-        });
-        await newSignup.save();
-      } else {
-        const newSignup = new User({
-          email: email,
-          account: {
-            username: username,
-          },
-          newsletter: newsletter,
-          token: token,
-          hash: hash,
-          salt: salt,
-          avatar: {},
-        });
-        await newSignup.save();
-      }
+      const newSignup = new User({
+        email: email,
+        account: {
+          username: username,
+        },
+        newsletter: newsletter,
+        token: token,
+        hash: hash,
+        salt: salt,
+        avatar: { secure_url: upLoad.secure_url },
+      });
+      await newSignup.save();
 
       const validSignup = {
         id: newSignup._id,
