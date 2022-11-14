1a3cbeb8f9d1e18c3c7d0e052dbe50e9574409b1
6dean <ferdjallahrockdean@gmail.com>
2022-11-14 09:29:00 +0100

    update

--- Routes/signup.js
+++ Routes/signup.js
@@ -12,7 +12,6 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
   try {
     const { username, email, password, newsletter } = req.body;
     const emailExist = await User.findOne({ email: email });
-    const { avatar } = req.files;
     if (!username) {
       return res.status(400).json({ message: "username needed" });
     }
@@ -25,7 +24,7 @@ router.post("/user/signup", fileUpload(), async (req, res) => {
       const hash = SHA256(salt + password).toString(encBase64);
       const token = uid2(16);
 
-      if (avatar) {
+      if (req.files.avatar) {
         const convertToBase64 = (file) => {
           return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
         };
